e4586ab878dfe5de01dc6786bc4dd785dc30c3ac
Yorvin Palacios Gomez <yorvin_20_1997@hotmail.com>
2019-10-24 15:04:12 -0500

    Actualizacion

--- app/src/main/java/com/example/cocheraapp/BusquedaActivity.java
+++ app/src/main/java/com/example/cocheraapp/BusquedaActivity.java
@@ -61,7 +61,7 @@ public class BusquedaActivity extends FragmentActivity implements OnMapReadyCall
         {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            
+
                 for
                 (Marker maker : realTimeMarkers)
                 {
